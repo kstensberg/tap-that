91d3ca14dca06a530ead29790d89f2ec7d5556b6
Kevin Stensberg <kstensberg@8-bitideas.com>
2013-01-20 02:00:31 -0800

    change the namespace once again to something java and android like

--- moai/android-host/build/facebook/gen/R.java.d
+++
@@ -1,9 +0,0 @@
-/home/kevin/src/tap-that/moai/android-host/build/facebook/gen/com/facebook/android/R.java \
- : /home/kevin/src/tap-that/moai/android-host/build/facebook/res/drawable-hdpi/close.png \
-/home/kevin/src/tap-that/moai/android-host/build/facebook/res/drawable-hdpi/facebook_icon.png \
-/home/kevin/src/tap-that/moai/android-host/build/facebook/res/drawable-xhdpi/close.png \
-/home/kevin/src/tap-that/moai/android-host/build/facebook/res/drawable-ldpi/close.png \
-/home/kevin/src/tap-that/moai/android-host/build/facebook/res/drawable-ldpi/facebook_icon.png \
-/home/kevin/src/tap-that/moai/android-host/build/facebook/res/drawable/close.png \
-/home/kevin/src/tap-that/moai/android-host/build/facebook/res/drawable/facebook_icon.png \
-/home/kevin/src/tap-that/moai/android-host/build/facebook/bin/AndroidManifest.xml \
--- moai/android-host/build/project/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/kevin/src/tap-that/moai/android-host/build/project/bin/AndroidManifest.xml : \
-/home/kevin/src/tap-that/moai/android-host/build/project/AndroidManifest.xml \
